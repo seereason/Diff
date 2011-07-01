edf8d8421542e048e7cb408d84951560fdadd89a
Kevin Charter <kevin.charter@acm.org>
2011-07-01 02:56:03 +0000

    Use a constant random number generator seed in the benchmark program.
    Having different random strings in each run of the benchmark
    program makes it harder to compare results from different runs.
    Use a fixed seed for now.
    
    At this point, here are the results for the 1000-bool benchmark on my
    machine using ghc 7.0.3 with no optimizations:
    
    mean: 208.7631 ms, lb 208.5287 ms, ub 208.9666 ms, ci 0.950
    std dev: 1.120883 ms, lb 966.4951 us, ub 1.364500 ms, ci 0.950
    found 3 outliers among 100 samples (3.0%)
      3 (3.0%) low mild
    variance introduced by outliers: 0.990%
    variance is unaffected by outliers
    
    Over the next few commits, I'll make some minor changes and show the
    change in benchmark times.

--- bench/bench.hs
+++ bench/bench.hs
@@ -9,11 +9,14 @@ import Data.Algorithm.Diff
 instance NFData DI
 
 main :: IO ()
-main = do
-  g <- newStdGen
-  let rbools = randoms g :: [Bool]
-  let (s1000_1, rbools1) = splitAt 1000 rbools
-  let (s1000_2, rbools2) = splitAt 1000 rbools1
-  s1000_1 `deepseq` s1000_2 `deepseq` defaultMain [
+main = doBenchMarks 37
+
+doBenchMarks :: Int -> IO ()
+doBenchMarks seed =
+  let rbools = randoms (mkStdGen seed) :: [Bool]
+      (s1000_1, rbools1) = splitAt 1000 rbools
+      (s1000_2, rbools2) = splitAt 1000 rbools1
+  in s1000_1 `deepseq` s1000_2 `deepseq` defaultMain [
     bgroup "diff bool lists" $ [bench "1000 bools" $ nf (getDiff s1000_1) s1000_2]
     ]
+    
