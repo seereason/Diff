5142fbe4c0e96645647a77020e8f1b05c92b05d7
unknown <unknown>
2012-12-11 21:53:21 +0000

    expose new constructors + funcs, cleanup tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,10 +1,31 @@
 module Main where
 
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
-
 import Data.Algorithm.Diff
 
-main = subTests >> diffTests
+
+main :: IO ()
+main = defaultMain [ testGroup "sub props" [
+                        slTest "empty in subs" prop_emptyInSubs,
+                        slTest "self in subs"  prop_selfInSubs,
+                        slTest "count subs"    prop_countSubs,
+                        slTest "every sub is a sub" prop_everySubIsSub,
+                        slTest2 "sub prop" prop_sub
+                     ],
+                     testGroup "diff props" [
+                        slTest "lcsEmpty" prop_lcsEmpty,
+                        slTest "lcsSelf" prop_lcsSelf,
+                        slTest2 "lcsBoth" prop_lcsBoth,
+                        slTest2 "recover first" prop_recoverFirst,
+                        slTest2 "recover second" prop_recoverSecond,
+                        slTest2 "lcs" prop_lcs
+                     ]
+                   ]
+
+slTest s t = testProperty s $ forAll shortLists   (t :: [Bool] -> Bool)
+slTest2 s t = testProperty s $ forAll2 shortLists (t :: [Bool] -> [Bool] -> Bool)
 
 -- We need some quick and dirty subsequence stuff for the diff tests,
 -- so we build that and some tests for it.
@@ -22,16 +43,6 @@ subs [] = [[]]
 subs (x:rest) = map (x:) restss ++ restss
   where restss = subs rest
 
-subTests :: IO ()
-subTests = do
-  mapM_ (quickCheck . forAll shortLists) ([
-    prop_emptyInSubs,
-    prop_selfInSubs,
-    prop_countSubs,
-    prop_everySubIsSub] :: [[Bool] -> Bool])
-  quickCheck $ forAll2 shortLists (prop_sub :: [Bool] -> [Bool] -> Bool)
-
-
 prop_emptyInSubs = elem [] . subs
 prop_selfInSubs xs = elem xs (subs xs)
 prop_countSubs xs = length (subs xs) == 2^(length xs)
@@ -47,19 +58,24 @@ diffLCS :: (Eq a) => [a] -> [a] -> [a]
 diffLCS xs ys = recoverLCS $ getDiff xs ys
 
 -- | Recovers the (longest) common subsequence from a diff.
-recoverLCS :: [(DI, a)] -> [a]
-recoverLCS = filterByDI (B==)
+recoverLCS :: [Diff a] -> [a]
+recoverLCS (Both x _ : xs) = x : recoverLCS xs
+recoverLCS (_ : xs) = recoverLCS xs
+recoverLCS [] = []
 
 -- | Recovers the first list from a diff.
-recoverFirst :: [(DI, a)] -> [a]
-recoverFirst = filterByDI (not . (S==))
+recoverFirst :: [Diff a] -> [a]
+recoverFirst (First x  : xs) = x : recoverFirst xs
+recoverFirst (Both x _ : xs) = x : recoverFirst xs
+recoverFirst (_ : xs) = recoverFirst xs
+recoverFirst [] = []
 
 -- | Recovers the second list from a diff.
-recoverSecond :: [(DI, a)] -> [a]
-recoverSecond = filterByDI (not . (F==)) 
-
-filterByDI :: (DI -> Bool) -> [(DI, a)] -> [a]
-filterByDI pred = map snd . filter (pred . fst)
+recoverSecond :: [Diff a] -> [a]
+recoverSecond (Second x  : xs) = x : recoverSecond xs
+recoverSecond (Both x _ : xs) = x : recoverSecond xs
+recoverSecond (_ : xs) = recoverSecond xs
+recoverSecond [] = []
 
 -- | Indicates whether a list is a longest common subsequence of two
 -- lists.
@@ -76,26 +92,21 @@ lenLCS _ [] = 0
 lenLCS (x:xs) (y:ys) | x == y = 1 + lenLCS xs ys
                      | otherwise = max (lenLCS (x:xs) ys) (lenLCS xs (y:ys))
 
-diffTests = do
-  mapM_ (quickCheck . forAll2 shortLists) ([
-    prop_recoverFirst,
-    prop_recoverSecond,
-    prop_lcs ] :: [[Bool] -> [Bool] -> Bool])
-  mapM_ (quickCheck. forAll shortLists) ([
-    prop_lcsEmpty,
-    prop_lcsSelf ] :: [[Bool] -> Bool])
 
 prop_recoverFirst xs ys = recoverFirst (getDiff xs ys) == xs
 prop_recoverSecond xs ys = recoverSecond (getDiff xs ys) == ys
 prop_lcs xs ys = isLCS (diffLCS xs ys) xs ys
 prop_lcsEmpty xs = diffLCS xs [] == [] && diffLCS [] xs == []
 prop_lcsSelf xs = diffLCS xs xs == xs
+prop_lcsBoth xs ys = all areMatch $ getDiff xs ys
+    where areMatch (Both x y) = x == y
+          areMatch _ = True
 
-
--- | Lists of no more than ten elements.
+-- | Lists of no more than twelve elements.
 shortLists :: (Arbitrary a) => Gen [a]
-shortLists = sized $ \n -> resize (min n 10) $ listOf arbitrary
+shortLists = sized $ \n -> resize (min n 12) $ listOf arbitrary
 
 -- | 'forAll' where the generator is used twice.
 forAll2 :: (Show a, Testable prop) => Gen a -> (a -> a -> prop) -> Property
 forAll2 gen f = forAll gen $ \x -> forAll gen (f x)
+
