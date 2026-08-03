9787bdfdb84bd3c9a0382c2fb3930b1b4bea2464
Xavier Góngora <ixbalanque@protonmail.ch>
2026-08-03 16:58:29 -0600

    Avoid discarding edge cases by using 1 as lower bound
    Co-authored-by: Facundo Domínguez <facundo.dominguez@tweag.io>

--- test/Test.hs
+++ test/Test.hs
@@ -165,8 +165,10 @@ prop_ppDiffR (DiffInput le ri) =
         utilDiff= unsafePerformIO (runDiff (unlines le) (unlines ri))
     in  cover 90 (haskDiff == utilDiff) "exact match" $
                 classify (haskDiff == utilDiff) "exact match" $
-                    not (null utilDiff) ==>
-                        (div ((length (lines haskDiff))*100) (length (lines utilDiff)) < 110) -- less than 10% bigger
+                    div
+                      (length (lines haskDiff)*100)
+                      (max 1 $ length $ lines utilDiff)
+                    < 110 -- less than 10% bigger
     where
       runDiff left right =
           do leftFile <- writeTemp left
