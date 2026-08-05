d6c363fc2817d1e4ec5959a3f9009738d74442ee
Xavier Góngora <xavier.gongora@tweag.io>
2026-08-05 12:22:14 -0600

    Remove unnecessary empty list equations from `getDiffBy` (revert #28)
    The optimization introduced in `dstep` (narrowing the wave front to only
    within bound nodes) solves the problem this additional equations addressed
    in https://github.com/seereason/Diff/commit/33bf8bc7f95a825b51f97e53848d39ef957546d9
    They are removed to avoid unnecessary complexity.

--- src/Data/Algorithm/Diff.hs
+++ src/Data/Algorithm/Diff.hs
@@ -517,8 +517,6 @@ getGroupedDiff = getGroupedDiffBy (==)
 -- | A form of 'getDiff' with no 'Eq' constraint. Instead, an equality predicate
 -- is taken as the first argument.
 getDiffBy :: (a -> b -> Bool) -> [a] -> [b] -> [PolyDiff a b]
-getDiffBy _ a [] = map First a
-getDiffBy _ [] b = map Second b
 getDiffBy eq a b = markup a b . reverse $ ses eq a b
     where markup (x:xs) (y:ys) ds
             | eq x y = Both x y : markup xs ys ds
